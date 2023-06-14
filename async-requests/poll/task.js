75d761d7f676f3f88cc3ca5e9a66a19f24bf758b
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-06-14 21:22:27 +0300

    async-requests homework done

---
+++ async-requests/poll/task.js
@@ -0,0 +1,36 @@
+let xhr = new XMLHttpRequest();
+const pollTitle = document.querySelector('#poll__title');
+const pollAnswers = document.querySelector('#poll__answers');
+
+xhr.addEventListener('readystatechange', () => {
+  if (xhr.readyState === xhr.DONE) {
+    const response = JSON.parse(xhr.responseText);
+    console.log(response);
+    pollTitle.textContent = response.data.title;
+
+    const answers = response.data.answers;
+    console.log(answers);
+
+    pollAnswers.innerHTML = '';
+
+    for (let answer of answers) {
+      const pollAnswer = document.createElement('button');
+      pollAnswer.classList.add('poll__answer');
+      pollAnswer.textContent = answer;
+      
+      pollAnswers.append(pollAnswer);
+    }
+  }
+});
+
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
+xhr.send();
+
+document.body.addEventListener('click', (e) => {
+  if (e.target.classList.contains('poll__answer')) {
+    alert('Спасибо, ваш голос засчитан!');
+
+    xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
+    xhr.send();
+  }
+})
---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('#form');
+const formData = new FormData(form);
+const progress = document.getElementById( 'progress' );
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.send(formData);
+
+    xhr.onprogress = function(e) {
+        progress.value = (e.loaded / e.total) * 100;
+    }
+});
